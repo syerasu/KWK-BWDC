b21e3d3831285e087c3310b1a6676ce9d3c137f0
syerasu <syerasu99@gmail.com>
2025-07-21 14:55:10 -0400

    pls commit

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,8 @@
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/KWK-BWDC/',    // right here! your repository name
   plugins: [svelte()],
+  base: '/KWK-BWDC/'
 })
