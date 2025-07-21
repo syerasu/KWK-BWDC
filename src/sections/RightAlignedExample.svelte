979b0981fb1a0593cbefa802ebc6782079dbfa3c
syerasu <syerasu99@gmail.com>
2025-07-21 18:40:09 -0400

    updated text boxes for pie chart

--- src/sections/RightAlignedExample.svelte
+++ src/sections/RightAlignedExample.svelte
@@ -93,3 +93,32 @@
         margin: 0px auto;
     }
 </style>
+  <Scroller layout="left">
+    {#snippet sticky()}
+      <div class="chart">
+        <Chart bind:chart {options} highcharts={Highcharts} />
+      </div>
+
+    {/snippet}
+
+    {#snippet scrolly()}
+      <ArticleText>
+        As seen in the pie chart, more than <strong>60%</strong> of the average household expenditure in the Bronx is allocated to housing and transportation costs, leaving less than <strong>40%</strong> for other essential needs like food and health services.
+      </ArticleText>
+
+      <ArticleText> 
+        We are privileged not to have to worry about having a roof over our heads or about where the money for necessities like food or healthcare will come from.      </ArticleText>     
+      <ArticleText>
+        The lack of worry about these basic needs allows us to focus on our education and personal growth, which is a luxury that we are lucky enough to take advantage of. <strong>This is a privilege not afforded to many.</strong>
+      </ArticleText>
+
+    {/snippet}
+  </Scroller>
+</div>
+
+<style>
+  .chart {
+    width: 100%;
+    margin: 0px auto;
+  }
+</style>
