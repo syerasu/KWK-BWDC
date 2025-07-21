30a10665bad8248a724c6372224efa4a6b07f661
syerasu <syerasu99@gmail.com>
2025-07-21 19:47:12 -0400

    adding footer and removing excess elements

--- src/App.svelte
+++ src/App.svelte
@@ -2,21 +2,12 @@
   import TitleSection from "./sections/TitleSection.svelte";
   import LeftAlignedExample from "./sections/LeftAlignedExample.svelte";
   import RightAlignedExample from "./sections/RightAlignedExample.svelte";
-  import ImageExample from "./sections/ImageExample.svelte";
-  import InteresectionObserverBasicExample from "./sections/InteresectionObserverBasicExample.svelte";
-  import DisappearingDuckExample from "./sections/DisappearingDuckExample.svelte";
-  import IntersectionObserverSectionCard from "./sections/IntersectionObserverSectionCard.svelte";
-  import IntersectionObserverListExample from "./sections/IntersectionObserverListExample.svelte";
+  import FooterSection from "./sections/FooterSection.svelte";
 </script>
 
 <main>
   <TitleSection />
   <RightAlignedExample />
   <LeftAlignedExample />
-  <ImageExample />
-
-  <IntersectionObserverSectionCard />
-  <InteresectionObserverBasicExample />
-  <DisappearingDuckExample />
-  <IntersectionObserverListExample />
+  <FooterSection />
 </main>
---
+++ src/lib/Footer.svelte
@@ -0,0 +1,58 @@
+<script>
+    let { title, subtitle } = $props();
+</script>
+
+<div class="title-card">
+    <div class="content">
+        <h1>{title}</h1>
+        <p>{subtitle}</p>
+    </div>
+</div>
+
+<style>
+    .title-card {
+        background: url('/pexels-brett-sayles-4734936.jpg') no-repeat center center;
+        background-size: cover;
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+        padding: 2rem;
+        box-sizing: border-box;
+        font-family: "Inter", sans-serif;
+    }
+
+    .content {
+        max-width: 700px;
+        background-color: #02022d ;
+        padding: 2rem;
+        border: 6px solid #D4AF37;
+        box-shadow: 16px 16px #FFECB3
+;
+    }
+
+    h1 {
+        font-size: 3rem;
+        margin: 0;
+        color: #f5f2ea;
+        text-shadow: 1px 1px 0 #FFECB3 
+;
+    }
+
+    p {
+        font-size: 1.3rem;
+        color: #f5f2ea;
+        margin-top: 1rem;
+    }
+
+    @media (max-width: 600px) {
+        h1 {
+            font-size: 2.2rem;
+        }
+
+        p {
+            font-size: 1.1rem;
+        }
+    }
+</style>
