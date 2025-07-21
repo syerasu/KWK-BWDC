ca4a49101a7ee5b7ea5c944b891c124d8fc00b5e
syerasu <syerasu99@gmail.com>
2025-07-21 15:09:33 -0400

    updated pie chart

--- src/sections/RightAlignedExample.svelte
+++ src/sections/RightAlignedExample.svelte
@@ -42,26 +42,26 @@
                 name: "Group",
                 data: [
                     {
-                        name: "Group 1",
-                        y: 151,
+                        name: "Food",
+                        y: 12.5,
                     },
                     {
-                        name: "Group 2",
+                        name: "Household",
                         sliced: true,
                         selected: true,
-                        y: 180,
+                        y: 37.5,
                     },
                     {
-                        name: "Group 3",
-                        y: 32,
+                        name: "Transportation",
+                        y: 37.5,
                     },
                     {
-                        name: "Group 4",
-                        y: 103,
+                        name: "Health Services",
+                        y: 15.5,
                     },
                     {
-                        name: "Group 5",
-                        y: 77,
+                        name: "Other",
+                        y: 20,
                     },
                 ],
             },
@@ -76,63 +76,13 @@
                 <Chart {options} highcharts={Highcharts} />
             </div>
             <p>
-                Here's an example chart using
-                <a href="https://www.highcharts.com/">Highcharts</a>!
+               (Data from
+                <a href="https://fred.stlouisfed.org/series/MHINY36005A052NCEN">The Federal Bank Reserve of St. Louis</a>)!
             </p>
-            <p>
-                📈 <strong>Highcharts</strong> is a super-flexible library for
-                creating all kinds of charts. See demos of different chart types
-                <a href="https://www.highcharts.com/demo">here</a>.
-            </p>
-            <p>
-                Since we're using Highcharts through Svelte, the syntax is a
-                little different from what you might see in the demos. But all
-                of Highcharts' functionality is available through the Highcharts
-                for Svelte package.
-            </p>
-            <p>
-                The configuration is done through the
-                <code>options</code> json object passed to the chart, which you'll
-                see in the source code for this template.
-            </p>
-            <p>
-                Use the
-                <a href="https://api.highcharts.com/highcharts/"
-                    >API reference</a
-                >
-                to understand what each element in the <code>options</code> object
-                does.
-            </p>
+    
         {/snippet}
 
         {#snippet scrolly()}
-            <ArticleText>
-                <strong>Welcome to the KWK Data Scrollytelling Template!</strong
-                >
-            </ArticleText>
-
-            <ArticleText>
-                This is a <strong>basic example</strong> of how you might create
-                a scrollytelling piece using Svelte and Highcharts.
-            </ArticleText>
-
-            <ArticleText>
-                You can use this template as a <strong>starting point</strong>
-                for your project.
-                <br /><br />
-                Or, if you want to build something from scratch, you can use it as
-                a <strong>reference</strong> for specific functionality.
-            </ArticleText>
-
-            <ArticleText>
-                This is <strong>just one way</strong> that scrollytelling can
-                look.
-                <br /><br />
-                <strong>
-                    If you use this template, be sure to modify it and make it
-                    your own!
-                </strong>
-            </ArticleText>
         {/snippet}
     </Scroller>
 </div>
